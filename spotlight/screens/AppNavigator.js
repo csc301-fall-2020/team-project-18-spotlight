a1a793569be2470229e1874e76526f902ebe645c
Zenghao <zenghao.wang@mail.utoronto.ca>
2020-11-11 14:50:26 -0500

    Format code

--- spotlight/screens/AppNavigator.js
+++ spotlight/screens/AppNavigator.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MainNavigator from './main/MainNavigator'
-import AuthNavigator from './authentication/AuthNavigator'
-import { Text, View } from 'react-native';
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MainNavigator from "./main/MainNavigator";
+import AuthNavigator from "./authentication/AuthNavigator";
+import { Text, View } from "react-native";
 
+const AppStack = createStackNavigator();
 
-const AppStack = createStackNavigator()
-
-
-// THIS SHOULD BE IN A TERNARY STATEMENT WITH LOGGED IN? 
+// THIS SHOULD BE IN A TERNARY STATEMENT WITH LOGGED IN?
 const AppNavigator = () => {
-    return (
-        <NavigationContainer>
-            <AppStack.Navigator
-            initialRouteName="Auth"
-            screenOptions={{
-                headerShown: false
-            }}
-            >
-                <AppStack.Screen name="Auth" component={AuthNavigator}/>
-                <AppStack.Screen name="Main" component={MainNavigator} />
-            </AppStack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator
+        initialRouteName="Auth"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <AppStack.Screen name="Auth" component={AuthNavigator} />
+        <AppStack.Screen name="Main" component={MainNavigator} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default AppNavigator;
--- spotlight/screens/main/gym/screens/MapScreen.js
+++ spotlight/screens/main/gym/screens/MapScreen.js
@@ -1,89 +1,88 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
-import MapView, {AnimatedRegion} from 'react-native-maps'
-import GetLocation from 'react-native-get-location'
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import MapView, { AnimatedRegion } from "react-native-maps";
+import GetLocation from "react-native-get-location";
 
 const MapScreen = () => {
-    // state = {
-    //     location: null,
-    //     loading: false,
-    // }
+  // state = {
+  //     location: null,
+  //     loading: false,
+  // }
 
-    // _requestLocation = () => {
-    //     this.setState({ loading: true, location: null });
+  // _requestLocation = () => {
+  //     this.setState({ loading: true, location: null });
 
-    //     GetLocation.getCurrentPosition({
-    //         enableHighAccuracy: true,
-    //         timeout: 150000,
-    //     })
-    //         .then(location => {
-    //             this.setState({
-    //                 location,
-    //                 loading: false,
-    //             });
-    //         })
-    //         .catch(ex => {
-    //             const { code, message } = ex;
-    //             console.warn(code, message);
-    //             if (code === 'CANCELLED') {
-    //                 Alert.alert('Location cancelled by user or by another request');
-    //             }
-    //             if (code === 'UNAVAILABLE') {
-    //                 Alert.alert('Location service is disabled or unavailable');
-    //             }
-    //             if (code === 'TIMEOUT') {
-    //                 Alert.alert('Location request timed out');
-    //             }
-    //             if (code === 'UNAUTHORIZED') {
-    //                 Alert.alert('Authorization denied');
-    //             }
-    //             this.setState({
-    //                 location: null,
-    //                 loading: false,
-    //             });
-    //         });
-    // }
-    
-    return (
-        <View style={styles.container}>
-            <Text style = {styles.header}>Gyms Nearby</Text>
-            <Text style = {styles.description}>Find a workout partner at your local gym</Text>
-            <MapView style={styles.map}/>
-        </View>
-        
-    );
+  //     GetLocation.getCurrentPosition({
+  //         enableHighAccuracy: true,
+  //         timeout: 150000,
+  //     })
+  //         .then(location => {
+  //             this.setState({
+  //                 location,
+  //                 loading: false,
+  //             });
+  //         })
+  //         .catch(ex => {
+  //             const { code, message } = ex;
+  //             console.warn(code, message);
+  //             if (code === 'CANCELLED') {
+  //                 Alert.alert('Location cancelled by user or by another request');
+  //             }
+  //             if (code === 'UNAVAILABLE') {
+  //                 Alert.alert('Location service is disabled or unavailable');
+  //             }
+  //             if (code === 'TIMEOUT') {
+  //                 Alert.alert('Location request timed out');
+  //             }
+  //             if (code === 'UNAUTHORIZED') {
+  //                 Alert.alert('Authorization denied');
+  //             }
+  //             this.setState({
+  //                 location: null,
+  //                 loading: false,
+  //             });
+  //         });
+  // }
 
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Gyms Nearby</Text>
+      <Text style={styles.description}>
+        Find a workout partner at your local gym
+      </Text>
+      <MapView style={styles.map} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 7,
-    },
-    map:{
-        ...StyleSheet.absoluteFillObject,
-        marginTop: "40%",
-        paddingBottom: "5%",
-        marginLeft: "5%",
-        marginRight: "5%",
-        justifyContent: 'flex-end',
-        borderWidth: 1,
-    },
-    header:{
-        fontFamily: "Raleway_600SemiBold",
-        fontSize: 30,
-        fontStyle: "normal",
-        textAlign: "left",
-        paddingTop: "20%",
-        paddingLeft: "3%",
-    },
-    description:{
-        fontFamily: "Raleway_600SemiBold",
-        marginBottom: "5%",
-        paddingLeft: "3%",
-        paddingBottom: "10%",
-    }
-  });
+  container: {
+    flex: 1,
+    padding: 7,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    marginTop: "40%",
+    paddingBottom: "5%",
+    marginLeft: "5%",
+    marginRight: "5%",
+    justifyContent: "flex-end",
+    borderWidth: 1,
+  },
+  header: {
+    fontFamily: "Raleway_600SemiBold",
+    fontSize: 30,
+    fontStyle: "normal",
+    textAlign: "left",
+    paddingTop: "20%",
+    paddingLeft: "3%",
+  },
+  description: {
+    fontFamily: "Raleway_600SemiBold",
+    marginBottom: "5%",
+    paddingLeft: "3%",
+    paddingBottom: "10%",
+  },
+});
 
 export default MapScreen;
-  
