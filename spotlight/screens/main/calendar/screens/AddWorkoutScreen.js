4019ea4c7aad053be86780e5c82b6681e6b5f41b
Jennifer Ruan <jennifer.ruan@gmail.com>
2020-11-12 18:12:47 -0500

    Added a workout form with a title and muscles input (these are saved to the component's state right now)

--- spotlight/screens/main/calendar/screens/AddWorkoutScreen.js
+++ spotlight/screens/main/calendar/screens/AddWorkoutScreen.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
-import { Text, View, Button, StyleSheet} from 'react-native';
+import { TouchableHighlight, TextInput, Text, View, Button, StyleSheet} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
+/**TODO: Move the "add exercise" form into another file to be its own component, each time the button is clicked the state with title, muscle type, reps and so on will be sent 
+ * back to this screen, which creates a list of these exercises.
+ */
 const AddWorkoutScreen = ({navigation}) => {
+    const [muscles, onChangeItem] = React.useState('abs')
+    const [title, onChangeText] = React.useState('Exercise Name');
     return (
         <View style={styles.container}>
             <Text style={styles.header}>
@@ -13,6 +19,32 @@ const AddWorkoutScreen = ({navigation}) => {
                 onPress={() =>
                     navigation.navigate('Calendar Stack')}
                 />
+
+            <TextInput 
+                style={styles.input}
+                onChangeText={text => onChangeText(text)}
+                value={title}/>
+
+            <DropDownPicker
+                items={[
+                    {label: 'Abs', value: 'abs', hidden: true},
+                    {label: 'Biceps', value: 'biceps'},
+                    {label: 'Glutes', value: 'glutes'}
+                ]}
+                defaultValue={ muscles }
+                containerStyle={{height: 35, width: 150}}
+                style={{backgroundColor: '#fafafa', marginTop: 5}}
+                itemStyle={{
+                    justifyContent: 'flex-start'
+                }}
+                dropDownStyle={{backgroundColor: '#fafafa'}}
+                onChangeItem={item => onChangeItem(item.value)}
+            />
+
+            <TouchableHighlight
+                style={styles.button}>
+                <Text style={styles.textStyle}>Add Exercise</Text>
+            </TouchableHighlight>
         </View>
     )
 }
@@ -48,18 +80,42 @@ const styles = StyleSheet.create({
         flex: 0.3,
         backgroundColor: "#d3d3d3",
         borderRadius: 10,
-      },
-      middle: {
+    },
+    middle: {
         flex: 0.3,
         backgroundColor: "#d3d3d3",
         borderRadius: 10,
-
-      },
-      bottom: {
+    },
+    bottom: {
         flex: 0.3,
         backgroundColor: "#d3d3d3",
         borderRadius: 10,
-      },
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    button: {
+        backgroundColor: "#F194FF",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        marginTop: 10
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    input: { 
+        marginTop: 5,
+        height: 30,
+        width: 150,
+        borderColor: '#d3d3d3', 
+        borderBottomWidth: 1
+    },
   });
 
 export default AddWorkoutScreen;
