74aa26c581a5d7e9a0782c6947d8800bacc12c70
renjiaq1 <jiaqing.ren@mail.utoronto.ca>
2020-11-16 13:45:36 -0500

    add the profile pages for the spotlight app

--- spotlight/screens/main/profile/ProfileScreen.js
+++ spotlight/screens/main/profile/ProfileScreen.js
@@ -1,25 +1,125 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Icon, StyleSheet, Text, View, Image, ImageBackground, Button, TextInput, TouchableOpacity } from 'react-native';
+import profilePic from "./images/profilePic.png"
+import book from "./images/book.png"
+import calender from "./images/calender.png"
+import friend from "./images/friend.png"
+import profile from "./images/profile.png"
+import editProfile from "./images/editProfile.png"
+
+const textinfo = "Hey, I’m Laura! I love cycling and my dog Francis. I’m usually at the gym every weekday morning, Lmk if you wanna do some workouts together!"
+
+
 
 
 const ProfileScreen = () => {
-    return(
+    return (
         <View style={styles.container}>
-            <Text>
-                {"Profile!"}
-            </Text>
+            <Image source={profilePic} style={styles.background} />
+            <TouchableOpacity style={styles.editProfile} onPress={() => { alert("you clicked me") }}>
+                <Image source={editProfile} />
+                <Text style={{ color: "white", position: "absolute", fontSize: 30 }} >
+                    {"Edit Profile"}
+                </Text>
+            </TouchableOpacity>
+
+            <View style={styles.info}>
+                <Text style={{ fontSize: 40 }} >
+                    {"Laura"}
+                </Text>
+                <Text style={styles.titleText} >
+                    {"F | 20"}
+                </Text>
+                <Text style={styles.titleText} >
+                    {textinfo}
+                </Text>
+
+
+            </View>
+
+            <View style={styles.buttonGroup}>
+                <TouchableOpacity style={styles.button} onPress={() => { alert("you clicked me") }}>
+                    <Image source={profile} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => { alert("you clicked me") }}>
+                    <Image source={book} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => { alert("you clicked me") }}>
+                    <Image source={calender} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => { alert("you clicked me") }}>
+                    <Image source={friend} />
+                </TouchableOpacity>
+            </View>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      alignItems: 'center'
     },
+    background: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: 400,
+      justifyContent: 'flex-end',
+    },
+    text: {
+      color: "white",
+      fontSize: 42,
+      fontWeight: "bold",
+      textAlign: "center",
+      backgroundColor: "#000000a0"
+    },
+    button: {
+      justifyContent: "center",
+      alignItems: 'center',
+  
+      width: 85,
+      height: 40
+    },
+    info: {
+      flexDirection: "column",
+      top: 350,
+      alignItems: 'center',
+      justifyContent: "center",
+      width: 300,
+      height: 300,
+      borderColor: 'black',
+      borderWidth: 1.2,
+      position: 'absolute',
+      // backgroundColor: 'white'
+      zIndex: 3,
+    },
+    editProfile: {
+      position: 'absolute',
+      flexDirection: "column",
+      alignItems: 'center',
+      justifyContent: "center",
+      top: 330,
+      zIndex: 10,
+      
+    },
+  
+    buttonGroup: {
+      flexDirection: "row",
+      bottom: 0,
+      position: 'absolute',
+      flex: 1,
+      justifyContent: 'flex-end',
+      marginBottom: 36
+    },
+    titleText: {
+      
+      fontSize: 20,
+      fontWeight: "bold"
+    }
   });
 
 export default ProfileScreen;
-  
