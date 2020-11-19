5f867130ea53b4891887bcbdb717eef60958c083
Jennifer Ruan <jennifer.ruan@gmail.com>
2020-11-18 23:24:07 -0500

    Exercises can now be added and removed from each body part

--- spotlight/screens/main/calendar/components/WorkoutDetails.js
+++ spotlight/screens/main/calendar/components/WorkoutDetails.js
@@ -1,9 +1,31 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableHighlight, Button} from "react-native";
+import { Text, View, StyleSheet, TextInput, TouchableHighlight, TouchableOpacity} from "react-native";
 
 const WorkoutDetails = (props) => {
-    const [muscles, onChangeItem] = React.useState('abs')
-    const [title, onChangeText] = React.useState('');
+    const [exercises, setExercises] = useState([
+        {
+            title: 'Tap me to remove',
+            weight: ':)',
+            reps: ''
+        }
+    ]);
+    const [title, setTitle] = useState('');
+    const [weight, setWeight] = useState('');
+    const [reps, setReps] = useState('');
+
+    const addExercise = () => {
+        var newExercise = {title: title, weight: weight, reps: reps}
+        setExercises(exercises => [...exercises, newExercise])
+
+        setTitle('')
+        setWeight('')
+        setReps('')
+    }
+
+    const removeExercise = (index) => {
+        setExercises(exercises => [...exercises.slice(0, index), ...exercises.slice(index+1)])
+    }
+    
     return (
         <View style={styles.container}>
           <Text style={styles.header}>
@@ -13,46 +35,61 @@ const WorkoutDetails = (props) => {
           <View
                 style={{
                     flexDirection: "row",
-                    height: 50,
+                    height: 20,
                     width: 300,
                 }}
                 >
                     <Text style={{flex: 0.6, textAlign: 'center'}}>Title</Text>
-                    <Text style={{flex: 0.2, textAlign: 'center'}}>Rep</Text>
                     <Text style={{flex: 0.2, textAlign: 'center'}}>Lbs</Text>
+                    <Text style={{flex: 0.2, textAlign: 'center'}}>Reps</Text>
             </View>
+
+            {exercises.map((exercise, index) => {
+                return (
+                    <TouchableOpacity
+                    style={styles.exercise}
+                    onPress={() => removeExercise(index)}
+                    >
+                        <Text style={{flex: 0.6, textAlign: 'center', color: "white",}}>{exercise.title}</Text>
+                        <Text style={{flex: 0.2, textAlign: 'center', color: "white",}}>{exercise.weight}</Text>
+                        <Text style={{flex: 0.2, textAlign: 'center', color: "white",}}>{exercise.reps}</Text>
+                    </TouchableOpacity>
+                );
+            })}
 
             <View
                 style={{
                     flexDirection: "row",
-                    height: 50,
+                    height: 30,
                     width: 300,
                 }}
                 >
                 <View style={{ flex: 0.6 }}>
                     <TextInput 
                         style={styles.input}
-                        onChangeText={text => onChangeText(text)}
-                        value={title}/>
+                        value={title}
+                        onChangeText={(text) => setTitle(text)}/>
                 </View>
                 <View style={{ flex: 0.2 }}>
                     <TextInput 
                         style={styles.input}
-                        onChangeText={text => onChangeText(text)}
-                        value={title}/>
+                        value={weight}
+                        onChangeText={(text) => setWeight(text)}/>
                 </View>
                 <View style={{ flex: 0.2 }}>
                     <TextInput 
                         style={styles.input}
-                        onChangeText={text => onChangeText(text)}
-                        value={title}/>
+                        value={reps}
+                        onChangeText={(text) => setReps(text)}/>
                 </View>
+                
             </View>
+                <TouchableHighlight
+                        style={styles.button}
+                        onPress={() => addExercise()}>
+                        <Text style={styles.textStyle}>Add</Text>
+                </TouchableHighlight>
 
-            <TouchableHighlight
-                style={styles.button}>
-                <Text style={styles.textStyle}>Add Exercise</Text>
-            </TouchableHighlight>
         </View>
       );
 }
@@ -60,18 +97,17 @@ const WorkoutDetails = (props) => {
 const styles = StyleSheet.create({
     header:{
         fontFamily: "Raleway_600SemiBold",
-        fontSize: 30,
+        fontSize: 24,
         fontStyle: "normal",
         textAlign: "left",
-        color: '#fff'
+        marginLeft: 5,
+        color: '#fff',
     },
     container: {
-        flex: 1,
         backgroundColor: '#d3d3d3',
-        alignItems: 'center',
         justifyContent: 'flex-start',
-        borderRadius: 5,
-        marginBottom: 10
+        borderRadius: 10,
+        marginBottom: 10,
       },
     input: { 
         borderColor: '#d3d3d3', 
@@ -83,16 +119,29 @@ const styles = StyleSheet.create({
     },
     button: {
         backgroundColor: "#000",
+        alignSelf: 'center',
+        justifyContent: 'center',
+        width: 40,
+        height: 20,
         borderRadius: 20,
-        padding: 10,
         elevation: 2,
-        marginTop: 10
+        marginTop: 10,
+        marginBottom: 5,
     },
     textStyle: {
         color: "white",
         fontWeight: "bold",
-        textAlign: "center"
+        textAlign: "center",
+        fontSize: 10,
     },
+    exercise: {
+        backgroundColor: '#888',
+        borderRadius: 10,
+        flexDirection: "row",
+        height: 20,
+        margin: 5,
+        marginBottom: 1,
+    }
 })
 
 export default WorkoutDetails;
--- spotlight/screens/main/calendar/screens/AddWorkoutScreen.js
+++ spotlight/screens/main/calendar/screens/AddWorkoutScreen.js
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'flex-start',
+      overflow: 'scroll',
     },
   });
 
