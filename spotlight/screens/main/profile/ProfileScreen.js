6f7891004836d7335793a4568829351790418a55
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2020-11-19 16:19:43 -0500

    I think that's the last merge conflict..?

--- spotlight/screens/main/profile/ProfileScreen.js
+++ spotlight/screens/main/profile/ProfileScreen.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import React, { useContext } from 'react';
-import { View, StyleSheet, Text} from 'react-native';
-import { Button } from 'react-native-paper';
-import { AuthContext } from '../../authentication/EmailContext/AuthProvider';
-
-const ProfileScreen = ({ navigation }) => {
-
-    const { emailLogout } = useContext(AuthContext);
-
-    return(
-        <View style={styles.container}>
-            <Text>
-                {"Profile!"}
-            </Text>
-            <Button
-            style={styles.back}
-            icon="logout" 
-            mode="contained" 
-            onPress = {() => emailLogout()}>
-                <Text style={{fontSize:15}}>Logout</Text>
-            </Button>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    back:{
-        marginTop: 20,
-        backgroundColor: 'salmon',
-        borderRadius:50, 
-        width:120
-    }
-  });
-
-export default ProfileScreen;
-  
-=======
 import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { Button } from "react-native-paper";
@@ -81,4 +37,3 @@ const styles = StyleSheet.create({
 });
 
 export default ProfileScreen;
->>>>>>> master
