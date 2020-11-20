c949c2d0da2cca802b9228ad2c512ce2c0a44c06
Samuel <samuel.vedrik@mail.utoronto.ca>
2020-11-20 17:33:01 -0500

    Added a function that adds users to firestore.

--- spotlight/services/userService.js
+++ spotlight/services/userService.js
@@ -1,3 +1,26 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+const db = firebase.firestore();
+
+const createNewUser = async (userID) => {
+  const data = {userID: userID};
+
+  try{
+    const userRef = db.collection("users").doc(userID);
+    userRef.get().then( 
+      (doc) => {
+        if(!doc.exists()){
+          doc.set(data);
+        }
+      }
+    )
+  }
+  catch (e){
+    console.log(e);
+  }
+}
+
 const getAllUsers = () => {
   return [];
 };
