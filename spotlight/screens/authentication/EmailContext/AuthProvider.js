6fa29d745345611d47ba4b76c67b31fcc821ed1f
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2020-11-20 14:40:54 -0500

    REFACTOR: Extracted firebase registration and logout functionality into authService.js file

--- spotlight/screens/authentication/EmailContext/AuthProvider.js
+++ spotlight/screens/authentication/EmailContext/AuthProvider.js
@@ -1,6 +1,10 @@
 import React, { createContext, useState } from "react";
 import firebase from "firebase";
-import { emailLogin } from "../../../services/authService";
+import {
+  emailLogin,
+  emailRegister,
+  emailLogout,
+} from "../../../services/authService";
 
 /**
  * This provider is created
@@ -18,36 +22,8 @@ export const AuthProvider = ({ children }) => {
         user,
         setUser,
         emailLogin,
-        emailRegister: async (email, password) => {
-          try {
-            await firebase
-              .auth()
-              .createUserWithEmailAndPassword(email, password);
-            // .then(() => {
-            //     console.log('User account created & signed in!');
-            // }).catch(error => {
-            //     if (error.code === 'auth/email-already-in-use') {
-            //     alert('That email address is already in use!');
-            //     }
-
-            //     if (error.code === 'auth/invalid-email') {
-            //     alert('That email address is invalid!');
-            //     }
-
-            //     console.log(error);
-            // });
-          } catch (e) {
-            alert(e);
-          }
-        },
-
-        emailLogout: async () => {
-          try {
-            await firebase.auth().signOut();
-          } catch (e) {
-            console.error(e);
-          }
-        },
+        emailRegister,
+        emailLogout,
       }}
     >
       {children}
--- spotlight/services/authService.js
+++ spotlight/services/authService.js
@@ -22,4 +22,35 @@ const emailLogin = async (email, password) => {
   }
 };
 
-export { emailLogin };
+const emailLogout = async () => {
+  try {
+    await firebase.auth.signOut();
+  } catch (e) {
+    // This should never happen.
+    console.log(e);
+  }
+};
+
+/**
+ * @param {string} email
+ * @param {string} password
+ * @returns {void}
+ * @throws {Error} With a message of what went wrong in the registration process.
+ */
+const emailRegister = async (email, password) => {
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+  } catch (e) {
+    console.log(e);
+    switch (e.code) {
+      case "auth/email-already-in-use":
+        throw new Error("Email already in use.");
+      case "auth/invalid-email":
+        throw new Error("Invalid email.");
+      case "auth/weak-password":
+        throw new Error("Please enter a stronger password.");
+    }
+  }
+};
+
+export { emailLogin, emailRegister, emailLogout };
