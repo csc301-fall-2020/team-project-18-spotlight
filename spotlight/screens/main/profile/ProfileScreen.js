bd3abf713794c2eaaa2e800b94596e1d2355fd01
Patrick Louis <patricklouisw@gmail.com>
2020-11-20 22:02:37 -0500

    update on profile

--- spotlight/screens/main/profile/ProfileScreen.js
+++ spotlight/screens/main/profile/ProfileScreen.js
@@ -1,21 +1,17 @@
-import React, { Component, useEffect, useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
-  Icon,
   StyleSheet,
   Text,
   View,
   Image,
-  ImageBackground,
-  ScrollView,
-  TextInput,
   TouchableOpacity,
 } from "react-native";
 import { Button } from "react-native-paper";
 import { AuthContext } from "../../authentication/EmailContext/AuthProvider";
-
 import profilePic from "./images/profilePic.png";
 import editProfile from "./images/editProfile.png";
+// import { LinearGradient } from 'expo-linear-gradient';
 
 const textinfo =
   "Hey, I’m Laura! I love cycling and my dog Francis. I’m usually at the gym every weekday morning, Lmk if you wanna do some workouts together!";
@@ -36,7 +32,7 @@ const ProfileScreen = ({ navigation }) => {
         onPress={() => {
           alert("you clicked me");
         }}
-      >
+      >           
         <Image source={editProfile} />
         <Text style={{ color: "white", position: "absolute", fontSize: 30 }}>
           {"Edit Profile"}
@@ -49,15 +45,15 @@ const ProfileScreen = ({ navigation }) => {
         <Text style={{ textAlign: "justify", fontSize: 16 }}>
           {profileInfo.description}
         </Text>
+        <Button
+          style={styles.back}
+          icon="logout"
+          mode="contained"
+          onPress={emailLogout}
+        >
+          <Text style={{ fontSize: 15 }}>Logout</Text>
+        </Button>
       </View>
-      <Button
-        style={styles.back}
-        icon="logout"
-        mode="contained"
-        onPress={emailLogout}
-      >
-        <Text style={{ fontSize: 15 }}>Logout</Text>
-      </Button>
     </SafeAreaView>
   );
 };
@@ -66,59 +62,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+
   },
   background: {
     position: "absolute",
     flex: 1,
     top: 30,
-    // bottom: 0,
-    // left: 0,
-    // right: 0,
+    width: '100%',
     height: 400,
     justifyContent: "flex-end",
   },
-  text: {
-    color: "white",
-    fontSize: 42,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000a0",
-  },
-  button: {
-    justifyContent: "center",
-    alignItems: "center",
-
-    width: 85,
-    height: 40,
-  },
   info: {
     flexDirection: "column",
-    top: 350,
+    top: 345,
     alignItems: "center",
     justifyContent: "center",
-    width: 300,
-    height: 300,
-    borderColor: "black",
-    borderWidth: 1.2,
-    position: "absolute",
+    borderColor: "grey",
+    borderWidth: 2,
+    borderBottomRightRadius: 40,
+    borderBottomLeftRadius: 40,
+    backgroundColor: 'white',
+    width: '95%',
+    height: '42%',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
     zIndex: 3,
     padding: 10,
+
   },
   editProfile: {
-    position: "absolute",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    top: 330,
+    top: 365,
     zIndex: 10,
-  },
-  buttonGroup: {
-    flexDirection: "row",
-    bottom: 0,
-    position: "absolute",
-    flex: 1,
-    justifyContent: "flex-end",
-    marginBottom: 36,
+    elevation: 3,
   },
   titleText: {
     fontSize: 20,
