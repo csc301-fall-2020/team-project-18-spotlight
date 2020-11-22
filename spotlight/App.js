bb043f7d19d4bebfd100816b50accbff9c67b6b3
Patrick Louis <patricklouisw@gmail.com>
2020-11-22 18:22:46 -0500

    Change Activity Indicators

--- spotlight/App.js
+++ spotlight/App.js
@@ -5,8 +5,10 @@ import * as firebase from "firebase";
 import { useFonts, Raleway_600SemiBold } from "@expo-google-fonts/raleway";
 import { AuthProvider } from "./screens/authentication/EmailContext/AuthProvider";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import {ActivityIndicator} from 'react-native';
+// import {ActivityIndicator} from 'react-native';
 import {firebaseConfig} from './config';
+import { ActivityIndicator, Colors } from 'react-native-paper';
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -16,7 +18,8 @@ const App = () => {
   let [fontsLoaded] = useFonts({ Raleway_600SemiBold });
 
   if (!fontsLoaded) {
-    return <ActivityIndicator size="large"/>;
+    return <ActivityIndicator size='large' animating={true} color={Colors.red800} />
+    // <ActivityIndicator size="large"/>;
   }
   return (
     <AuthProvider>
