4e794c847e54174bad4cc4b875199e5396d88f08
renjiaq1 <jiaqing.ren@mail.utoronto.ca>
2020-11-22 15:50:55 -0500

    add text input to edit profile screen

--- spotlight/screens/main/profile/EditProfileScreen.js
+++ spotlight/screens/main/profile/EditProfileScreen.js
@@ -1,119 +1,144 @@
 import React, { useState, useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    TextInput
 } from "react-native";
 import { Button } from "react-native-paper";
 import { AuthContext } from "../../authentication/EmailContext/AuthProvider";
 import profilePic from "./images/profilePic.png";
 import editProfile from "./images/editProfile.png";
-// import { LinearGradient } from 'expo-linear-gradient';
+
+
 
 const textinfo =
-  "Hey, I’m Laura! I love cycling and my dog Francis. I’m usually at the gym every weekday morning, Lmk if you wanna do some workouts together!";
+    "Hey, I’m Laura! I love cycling and my dog Francis. I’m usually at the gym every weekday morning, Lmk if you wanna do some workouts together!";
 
 const EditProfileScreen = ({ navigation }) => {
-  const [profileInfo, setInfo] = useState({
-    name: "Laura",
-    gender: "F | 20",
-    description: textinfo,
-  });
-  const { emailLogout } = useContext(AuthContext);
+    const [profileInfo, setInfo] = useState({
+        email: "laura.la@utoronto.ca",
+        phone: "647-666-6666",
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image source={profilePic} style={styles.background} />
-      <TouchableOpacity
-        style={styles.editProfile}
-        onPress={() => {
-          alert("you clicked me");
-        }}
-      >           
-        <Image source={editProfile} />
-        <Text style={{ color: "white", position: "absolute", fontSize: 30 }}>
-          {"Edit Profile"}
-        </Text>
-      </TouchableOpacity>
+        password: "123456",
 
-      <View style={styles.info}>
-        <Text style={{ fontSize: 40 }}>{profileInfo.name}</Text>
-        <Text style={styles.titleText}>{profileInfo.gender}</Text>
-        <Text style={{ textAlign: "justify", fontSize: 16 }}>
-          {profileInfo.description}
-        </Text>
-        <Button
-          style={styles.back}
-          icon="logout"
-          mode="contained"
-          onPress={emailLogout}
-        >
-          <Text style={{ fontSize: 15 }}>Logout</Text>
-        </Button>
-      </View>
-    </SafeAreaView>
-  );
+        nickname: "Lara",
+        name: "Laura",
+        birthday: "2000-01-01",
+        gender: "F",
+        age_lower: 20,
+        age_upper: 25,
+        description: textinfo,
+
+    });
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.changePassword}>
+                <View style={styles.textInput}>
+                    <Text>
+                        Change password:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+
+                <View style={styles.textInput}>
+                    <Text>
+                        Confirm password:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+
+                <Button>
+                    Save
+                </Button>
+
+            </View>
+
+
+            <View style={styles.changeInfo}>
+                <View style={styles.textInput}>
+                    <Text>
+                        Change nickname:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+
+                <View style={styles.textInput}>
+                    <Text>
+                        Change name:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+
+                <View style={styles.textInput}>
+                    <Text>
+                        Change birthday:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+
+                <View style={styles.textInput}>
+                    <Text>
+                        Change gender:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+
+                <View style={styles.textInput}>
+                    <Text>
+                        Change description:
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                    />
+                </View>
+
+                
+                <Button>
+                    Save
+                </Button>
+
+            </View>
+        </View>
+
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-
-  },
-  background: {
-    position: "absolute",
-    flex: 1,
-    top: 0,
-    width: '100%',
-    height: 460,
-    justifyContent: "flex-end",
-  },
-  info: {
-    flexDirection: "column",
-    top: 345,
-    alignItems: "center",
-    justifyContent: "center",
-    borderColor: "grey",
-    borderWidth: 2,
-    borderBottomRightRadius: 40,
-    borderBottomLeftRadius: 40,
-    backgroundColor: 'white',
-    width: '95%',
-    height: '42%',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
+    container: {
+        justifyContent: "center",
+        alignItems: "center"
     },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
-    zIndex: 3,
-    padding: 10,
-
-  },
-  editProfile: {
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    top: 365,
-    zIndex: 10,
-    elevation: 3,
-  },
-  titleText: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  back: {
-    marginTop: 20,
-    backgroundColor: "salmon",
-    borderRadius: 50,
-    width: 120,
-  },
+    textInput: {
+    },
+    input: {
+        borderWidth: 1,
+        width: 300,
+    },
+    changePassword: {
+        top: 80,
+    },
+    changeInfo: {
+        top: 100,
+    }
 });
 
 export default EditProfileScreen;
+
+
