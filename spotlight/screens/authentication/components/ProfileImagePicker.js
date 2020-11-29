188cbc5f25119524434204ca2e90f038f631d41b
Samuel <samuel.vedrik@mail.utoronto.ca>
2020-11-29 16:53:01 -0500

    Components for profile image picker

---
+++ spotlight/screens/authentication/components/ProfileImagePicker.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect, useContext } from "react";
+import {
+  View,
+  TouchableHighlight,
+  Text,
+  Image,
+  StyleSheet,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import { Avatar } from "react-native-paper";
+import { uploadUserImage } from "../../../services/userService";
+
+const ProfileImagePicker = ({ setImageURL }) => {
+  const [image, setImage] = useState("../../../assets/profile_picture.png");
+
+  useEffect(() => {
+    (async () => {
+      await ImagePicker.requestCameraRollPermissionsAsync();
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    try {
+      const pickerResult = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [3, 3],
+      });
+      if (!pickerResult.cancelled) {
+        await handleImagePicked(pickerResult);
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  const handleImagePicked = async (pickerResult) => {
+    const uploadURL = await uploadUserImage(pickerResult.uri);
+    // Uses the image in local.
+    await setImage(pickerResult.uri);
+    // Gives download link to parent to store in database.
+    await setImageURL(uploadURL);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Avatar.Image source={{ uri: image }} />
+      <TouchableHighlight
+        onPress={pickImage}
+        underlayColor={"#DDDDDD"}
+        style={{ borderRadius: 3 }}
+      >
+        <View style={styles.pickImageContainer}>
+          <Text style={styles.pickImage}>{"Change profile picture"}</Text>
+        </View>
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "1%",
+  },
+  image: {},
+  pickImage: {
+    fontSize: 15,
+    color: "blue",
+    fontWeight: "bold",
+  },
+  pickImageContainer: {
+    marginTop: "1%",
+  },
+});
+
+export { ProfileImagePicker };
