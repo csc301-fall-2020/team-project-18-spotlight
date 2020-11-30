684c8929d1c0705e97ddcb9e068f593829b5ae24
Jennifer Ruan <jennifer.ruan@gmail.com>
2020-11-30 17:18:35 -0500

    Users can now edit, save, cancel, and clear notes they wanna add for any body part for a workout

--- spotlight/screens/main/calendar/components/WorkoutDetails.js
+++ spotlight/screens/main/calendar/components/WorkoutDetails.js
@@ -9,7 +9,199 @@ import {
 } from "react-native";
 
 const WorkoutDetails = (props) => {
-  const [exercises, setExercises] = useState([
+  const [savedNotes, setSavedNotes] = useState(
+    "No notes yet. Press edit to add some!"
+  );
+  const [editNotes, setEditNotes] = useState("");
+  const [editMode, toggleEditMode] = useState(false);
+
+  const saveNotes = () => {
+    setSavedNotes(editNotes);
+    setEditNotes("");
+    toggleEditMode(false);
+  };
+
+  const cancelEdit = () => {
+    setEditNotes("");
+    toggleEditMode(false);
+  };
+
+  const enterEdit = () => {
+    setEditNotes(savedNotes);
+    toggleEditMode(true);
+  };
+
+  return (
+    <View style={styles.container}>
+      {editMode && (
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              height: 50,
+              width: 320,
+            }}
+          >
+            <View style={{ flex: 0.6 }}>
+              <Text style={styles.header}>{props.muscle}</Text>
+            </View>
+            <View style={{ flex: 0.05 }}>
+              <TouchableHighlight
+                style={styles.cancel}
+                onPress={() => cancelEdit()}
+              >
+                <Text style={styles.textStyle}>x</Text>
+              </TouchableHighlight>
+            </View>
+            <View style={{ flex: 0.2 }}>
+              <TouchableHighlight
+                style={styles.clear}
+                onPress={() => setEditNotes("")}
+              >
+                <Text style={styles.textStyle}>Clear</Text>
+              </TouchableHighlight>
+            </View>
+            <View style={{ flex: 0.1 }}>
+              <TouchableHighlight
+                style={styles.button}
+                onPress={() => saveNotes()}
+              >
+                <Text style={styles.textStyle}>Save</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+
+          <View
+            style={{
+              margin: 10,
+            }}
+          >
+            <TextInput
+              style={styles.input}
+              value={editNotes}
+              onChangeText={(text) => setEditNotes(text)}
+            />
+          </View>
+        </View>
+      )}
+
+      {!editMode && (
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              height: 50,
+              width: 320,
+            }}
+          >
+            <View style={{ flex: 0.8 }}>
+              <Text style={styles.header}>{props.muscle}</Text>
+            </View>
+            <View style={{ flex: 0.2 }}>
+              <TouchableHighlight
+                style={styles.button}
+                onPress={() => enterEdit()}
+              >
+                <Text style={styles.textStyle}>Edit</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+
+          <View style={styles.notesBox}>
+            <Text style={styles.notes}>{savedNotes}</Text>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    fontFamily: "Raleway_600SemiBold",
+    fontSize: 24,
+    fontStyle: "normal",
+    textAlign: "left",
+    marginLeft: 10,
+    marginTop: 10,
+    color: "#000",
+  },
+  container: {
+    backgroundColor: "#d3d3d3",
+    justifyContent: "center",
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  input: {
+    borderColor: "#d3d3d3",
+    borderWidth: 1,
+    backgroundColor: "#fafafa",
+    borderRadius: 5,
+    height: 30,
+    margin: 5,
+  },
+  button: {
+    backgroundColor: "#000",
+    alignSelf: "center",
+    justifyContent: "center",
+    width: 40,
+    height: 20,
+    borderRadius: 20,
+    elevation: 2,
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  cancel: {
+    backgroundColor: "#A20A0A",
+    alignSelf: "center",
+    justifyContent: "center",
+    width: 25,
+    height: 20,
+    borderRadius: 20,
+    elevation: 2,
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  clear: {
+    backgroundColor: "#888",
+    alignSelf: "center",
+    justifyContent: "center",
+    width: 45,
+    height: 20,
+    borderRadius: 20,
+    elevation: 2,
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 9,
+  },
+  exercise: {
+    backgroundColor: "#888",
+    borderRadius: 10,
+    flexDirection: "row",
+    height: 20,
+    margin: 5,
+    marginBottom: 1,
+  },
+  notesBox: {
+    margin: 10,
+    backgroundColor: "#888",
+    borderRadius: 10,
+    flexDirection: "row",
+    flex: 1,
+    flexWrap: "wrap",
+  },
+  notes: {
+    margin: 10,
+    color: "white",
+  },
+});
+
+/*const [exercises, setExercises] = useState([
     {
       title: "Tap me to remove",
       weight: ":)",
@@ -19,7 +211,7 @@ const WorkoutDetails = (props) => {
   const [title, setTitle] = useState("");
   const [weight, setWeight] = useState("");
   const [reps, setReps] = useState("");
-
+  
   const addExercise = () => {
     var newExercise = { title: title, weight: weight, reps: reps };
     setExercises((exercises) => [...exercises, newExercise]);
@@ -29,22 +221,18 @@ const WorkoutDetails = (props) => {
     setReps("");
   };
 
-  const removeExercise = (index) => {
+      const removeExercise = (index) => {
     setExercises((exercises) => [
       ...exercises.slice(0, index),
       ...exercises.slice(index + 1),
     ]);
   };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>{props.muscle}</Text>
-
-      <View
+  
+        <View
         style={{
           flexDirection: "row",
           height: 20,
-          width: 300,
+          width: 320,
         }}
       >
         <Text style={{ flex: 0.6, textAlign: "center" }}>Title</Text>
@@ -70,12 +258,12 @@ const WorkoutDetails = (props) => {
           </TouchableOpacity>
         );
       })}
-
-      <View
+      
+       <View
         style={{
           flexDirection: "row",
           height: 30,
-          width: 300,
+          width: 320,
         }}
       >
         <View style={{ flex: 0.6 }}>
@@ -99,62 +287,6 @@ const WorkoutDetails = (props) => {
             onChangeText={(text) => setReps(text)}
           />
         </View>
-      </View>
-      <TouchableHighlight style={styles.button} onPress={() => addExercise()}>
-        <Text style={styles.textStyle}>Add</Text>
-      </TouchableHighlight>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  header: {
-    fontFamily: "Raleway_600SemiBold",
-    fontSize: 24,
-    fontStyle: "normal",
-    textAlign: "left",
-    marginLeft: 5,
-    color: "#fff",
-  },
-  container: {
-    backgroundColor: "#d3d3d3",
-    justifyContent: "flex-start",
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  input: {
-    borderColor: "#d3d3d3",
-    borderWidth: 1,
-    backgroundColor: "#fafafa",
-    borderRadius: 5,
-    height: 30,
-    margin: 5,
-  },
-  button: {
-    backgroundColor: "#000",
-    alignSelf: "center",
-    justifyContent: "center",
-    width: 40,
-    height: 20,
-    borderRadius: 20,
-    elevation: 2,
-    marginTop: 10,
-    marginBottom: 5,
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-    fontSize: 10,
-  },
-  exercise: {
-    backgroundColor: "#888",
-    borderRadius: 10,
-    flexDirection: "row",
-    height: 20,
-    margin: 5,
-    marginBottom: 1,
-  },
-});
+      </View>*/
 
 export default WorkoutDetails;
