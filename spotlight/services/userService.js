1bc2727fd54cd8188cb33185313a5054e413d74e
Samuel <samuel.vedrik@mail.utoronto.ca>
2020-12-02 00:03:05 -0500

    Emergency fixes for services

--- spotlight/services/userService.js
+++ spotlight/services/userService.js
@@ -2,6 +2,9 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import uuid from "uuid";
 
+/**
+DEPRECATED, DO NOT USE 
+ */
 const createNewUser = async (userID) => {
   console.log("Trying to add user", userID);
   const data = { userID: userID };
@@ -89,6 +92,7 @@ const updateUserInfo = async (userData, userID) => {
 const processUserDoc = (userDoc) => {
   const user = userDoc.data();
   return {
+    userID: userDoc.id,
     address: user.address,
     age: user.age,
     city: user.city,
@@ -101,7 +105,6 @@ const processUserDoc = (userDoc) => {
     phoneNumber: user.phoneNumber,
     profilePicture: user.profilePicture,
     province: user.province,
-    userID: user.userID,
     username: user.username,
     zip: user.zip,
     bio: user.bio,
